fec62b8fb2c5c968fec9aec31392c7bb34976f9e
Chuan Yu <chuanyucs@gmail.com>
2019-04-11 00:30:04 -0700

    refactor jsonrpc configuration

--- util/src/main/java/com/cy/finance/util/configuration/RpcConfiguration.java
+++ util/src/main/java/com/cy/finance/util/configuration/RpcConfiguration.java
@@ -1,12 +1,11 @@
-package com.cy.finance.seller.configuration;
+package com.cy.finance.util.configuration;
 
-import com.cy.finance.api.ProductRpc;
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcClientProxyCreator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.MalformedURLException;
@@ -17,20 +16,20 @@ import java.net.URL;
  * @Date: 4/10/19 10:25 PM
  */
 @Configuration
-@ComponentScan(basePackageClasses = {ProductRpc.class})
 public class RpcConfiguration {
 
   private static Logger LOG = LoggerFactory.getLogger(RpcConfiguration.class);
 
   @Bean
-  public AutoJsonRpcClientProxyCreator rpcClientProxyCreator(@Value("${rpc.manager.url}") String url) {
+  @ConditionalOnProperty(value = {"rpc.client.url","rpc.client.basePackage"})
+  public AutoJsonRpcClientProxyCreator rpcClientProxyCreator(@Value("${rpc.client.url}") String url, @Value("${rpc.client.basePackage}") String basePackage) {
     AutoJsonRpcClientProxyCreator creator = new AutoJsonRpcClientProxyCreator();
     try {
       creator.setBaseUrl(new URL(url));
     } catch (MalformedURLException e) {
       LOG.error("error create rpc", e);
     }
-    creator.setScanPackage(ProductRpc.class.getPackage().getName());
+    creator.setScanPackage(basePackage);
     return creator;
   }
 }
